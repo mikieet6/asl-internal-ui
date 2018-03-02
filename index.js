71fff54f3ce4f29ae50202cb9fccfd9699b1b0c5
Leonard Martin <leonard.martin@gmail.com>
2018-03-02 10:45:35 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const App = require('./lib/app');
+const settings = require('./config');
+
+const server = App(settings).listen(settings.port, (err, result) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+process.on('SIGINT', () => {
+  if (server.listening) {
+    console.log('Attempting to exit gracefully.');
+    server.close(() => {
+      console.log('Server closed. Quitting.');
+      process.exit();
+    });
+  }
+});
