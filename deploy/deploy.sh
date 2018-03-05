e7c0c3f2b08fdbf102283ff2c14afdec8ebb72f0
Leonard Martin <leonard.martin@gmail.com>
2018-03-05 14:12:39 +0000

    Fix deploy file paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,12 +3,10 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-export SERVICE_NAME=inspector-ui
-
 kd --insecure-skip-tls-verify \
-  -f deploy/app-deployment.yaml \
-  -f deploy/app-service.yaml \
-  -f deploy/app-ingress.yaml \
+  -f deploy/deployment.yaml \
+  -f deploy/service.yaml \
+  -f deploy/ingress.yaml \
   -f deploy/network-policy.yaml \
   -f deploy/redis-deployment.yaml \
   -f deploy/redis-service.yaml \
