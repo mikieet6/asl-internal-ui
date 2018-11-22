1a23c1ec5ce36973805a9a550f1c11fb2d454040
Leonard Martin <leonard.martin@gmail.com>
2018-11-22 14:37:28 +0000

    Revive dashboard
    This was supposed to be just using the feedback page, but updating to the new pages broke the existing dashboard/task list, so that needed to be fixed too to get anything to work.

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,5 +1,5 @@
 const { page } = require('@asl/pages');
-const workflow = require('../../lib/middleware/workflow');
+const taskList = require('@asl/pages/pages/common/routers/task-list');
 
 module.exports = settings => {
   const app = page({
@@ -7,53 +7,12 @@ module.exports = settings => {
     root: __dirname
   });
 
-  app.use(workflow(settings));
-
-  app.get('/', (req, res, next) => {
-    req.workflow('/')
-      .then(response => {
-        res.locals.static.schema = {
-          type: {
-            show: true,
-            accessor: 'data'
-          },
-          created_at: {
-            show: true
-          },
-          status: {
-            show: true
-          },
-          id: {
-            show: true
-          }
-        };
-        res.locals.datatable = {
-          data: {
-            rows: response.json.data
-              .sort((a, b) => a.created_at < b.created_at ? 1 : -1)
-          }
-        };
-      })
-      .then(() => next())
-      .catch(next);
-  });
-
   app.get('/', (req, res, next) => {
     res.locals.static.profile = req.user.profile;
     next();
   });
 
-  app.post('/task/:id', (req, res, next) => {
-    const params = {
-      method: 'PUT',
-      json: { status: 'resolved' }
-    };
-    req.workflow(`/${req.params.id}/status`, params)
-      .then(() => {
-        res.redirect('/');
-      })
-      .catch(next);
-  });
+  app.use(taskList());
 
   return app;
 };
