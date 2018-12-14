9578c355776b604f54c6b66ce680975868d0889a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-12-14 15:50:20 +0000

    add search panel to root dashboard

--- pages/dashboard/views/index.jsx
+++ pages/dashboard/views/index.jsx
@@ -1,23 +1,67 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import {
-  Link,
-  Snippet,
-  Header
-} from '@asl/components';
+import { Header, Link, Search, Snippet } from '@asl/components';
 import Tasklist from '@asl/pages/pages/task/list/views/tasklist';
+
+const models = [
+  'establishments',
+  'people',
+  'projects'
+];
 
 const Index = ({
   profile: {
-    firstName,
-    establishments
+    firstName
   },
-  tasks
+  searchType
 }) => (
   <Fragment>
-    <Header
-      title={<Snippet name={firstName}>pages.dashboard.greeting</Snippet>}
-    />
+    <Header title={<Snippet name={firstName}>pages.dashboard.greeting</Snippet>} />
+
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-full">
+        <div className="search-panel">
+          <h2><Snippet>searchPanel.title</Snippet></h2>
+
+          <ul className="search-type">
+            { models.map((model, index) => (
+              <li key={index}>
+                <a href={`/?searchType=${model}`} className={searchType === model ? 'active' : ''}>
+                  <Snippet>{`searchPanel.${model}.label`}</Snippet>
+                </a>
+              </li>
+            )) }
+          </ul>
+
+          <Search hideLabel={true}>
+            <div className="view-all-link">
+              { searchType === 'establishments' &&
+                <Link
+                  page="establishment.list"
+                  label={<Snippet>searchPanel.establishments.viewAll</Snippet>}
+                  className="view-all-establishments"
+                />
+              }
+              { searchType === 'people' &&
+                <Link
+                  page="profile.list"
+                  label={<Snippet>searchPanel.people.viewAll</Snippet>}
+                  className="view-all-people"
+                />
+              }
+              { searchType === 'projects' &&
+                <Link
+                  page="project.list"
+                  label={<Snippet>searchPanel.projects.viewAll</Snippet>}
+                  className="view-all-projects"
+                />
+              }
+            </div>
+          </Search>
+        </div>
+      </div>
+    </div>
+
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-full">
         <h2><Snippet>pages.dashboard.tasks</Snippet></h2>
@@ -27,5 +71,7 @@ const Index = ({
   </Fragment>
 );
 
-const mapStateToProps = ({ static: { profile, tasks } }) => ({ profile, tasks });
+const mapStateToProps = ({
+  static: { profile, searchType }
+}) => ({ profile, searchType });
 export default connect(mapStateToProps)(Index);
