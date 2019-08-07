ca3429f7508cf5b728b7baf2d90eacb6ca3d76d8
Joe Fitter <joefitter@hotmail.com>
2019-08-07 08:31:59 +0100

    Update tasklist - now controls own props Bumped pages

--- pages/dashboard/views/index.jsx
+++ pages/dashboard/views/index.jsx
@@ -4,27 +4,13 @@ import { Header, Snippet } from '@asl/components';
 import Tasklist from '@asl/pages/pages/task/list/views/tasklist';
 import SearchPanel from '../../components/search-panel';
 
-const Index = ({ profile, searchType, progress, tabs }) => (
+const Index = ({ profile, searchType }) => (
   <Fragment>
     <Header title={<Snippet name={profile.firstName}>pages.dashboard.greeting</Snippet>} />
-
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <SearchPanel searchType={searchType} action={`/search/${searchType}`} />
-      </div>
-    </div>
-
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h2><Snippet>pages.dashboard.tasks</Snippet></h2>
-        <Tasklist tabs={tabs} progress={progress} />
-      </div>
-    </div>
+    <SearchPanel searchType={searchType} action={`/search/${searchType}`} />
+    <h2><Snippet>pages.dashboard.tasks</Snippet></h2>
+    <Tasklist />
   </Fragment>
 );
 
-const mapStateToProps = ({
-  static: { profile, searchType, progress, tabs }
-}) => ({ profile, searchType, progress, tabs });
-
-export default connect(mapStateToProps)(Index);
+export default connect(({ static: { profile, searchType } }) => ({ profile, searchType }))(Index);
