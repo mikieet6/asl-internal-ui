28a7b44fb3efdcd788899597c3874319c756355f
Leonard Martin <leonard.martin@gmail.com>
2019-08-23 13:41:13 +0100

    Add metrics dashboard
    Adds output for a couple of headline figures and a breakdown of completed tasks by type for a specified date range.
    
    Remove the UI for NTS summaries as that was a bit of a spike, but leave the rendering code in place.

---
+++ pages/reporting/content/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'project-application': 'PPL applications',
+  'project-amendment': 'PPL amendments',
+  'pil-grant': 'PIL applications',
+  'pil-update': 'PIL amendments',
+  'pil-revoke': 'PIL revocations',
+  'role-create': 'Named people assignments',
+  'role-delete': 'Named people revocations',
+  'place-update': 'Approved area amendments',
+  'place-create': 'Approved area additions',
+  'place-delete': 'Approved area deletions',
+  'profile-update': 'Profile updates',
+  date: {
+    all: 'All',
+    week: 'This week',
+    month: 'This month',
+    year: 'This year'
+  },
+  breadcrumbs: {
+    metrics: 'Performance metrics'
+  }
+};
