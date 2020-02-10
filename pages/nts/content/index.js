e8d67472739393dbc7580c4aa8489bbd99a31e4b
Leonard Martin <leonard.martin@gmail.com>
2020-02-10 11:45:27 +0000

    Fix typo in NTS download page
    Also remove the incorrect bit about them being PDFs, because they're not PDFs.

--- pages/nts/content/index.js
+++ pages/nts/content/index.js
@@ -2,11 +2,11 @@ module.exports = {
   page: {
     title: 'Download non-technical summaries by year',
     subTitle: 'Non-technical summaries',
-    summary: `These will download as a .zip folder containing the PDFs of the non-technical summaries.`
+    summary: `These will download as a .zip folder containing the non-technical summaries.`
   },
   nts: {
     link: {
-      label: 'Non-techincal summaries published in {{year}} (.zip)'
+      label: 'Non-technical summaries published in {{year}} (.zip)'
     }
   }
 };
