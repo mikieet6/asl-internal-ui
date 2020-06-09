c36047624110b7fff00648bae5596d0a516151ad
Leonard Martin <leonard.martin@gmail.com>
2020-06-09 14:07:50 +0100

    WIP - Request streaming
    Streams data directly from the metrics service to a csv response without consuming the dataset into memory.

--- pages/downloads/reports/index.js
+++ pages/downloads/reports/index.js
@@ -1,12 +1,23 @@
+const fetch = require('r2');
+const { pipeline } = require('stream');
+const ndjson = require('ndjson');
+const through = require('through2');
+
 const { Router } = require('express');
 const csv = require('csv-stringify');
 const conditionsReportMapper = require('./conditions-report-mapper');
 
-const process = report => data => {
-  if (report === 'ppl-conditions') {
-    return conditionsReportMapper(data);
+const process = report => (data, encoding, callback) => {
+  try {
+    switch (report) {
+      case 'ppl-conditions':
+        return callback(null, conditionsReportMapper(data));
+      default:
+        return callback(null, data);
+    }
+  } catch(e) {
+    return callback(e);
   }
-  return data;
 };
 
 module.exports = settings => {
@@ -14,17 +25,30 @@ module.exports = settings => {
 
   router.get('/', (req, res, next) => {
     const report = req.params.report;
-    req.api(`/reports/${report}`)
-      .then(result => result.json.data)
-      .then(process(report))
-      .then(data => {
-        res.attachment(`${report}.csv`);
-        const stringifier = csv({ header: true });
-        data.forEach(row => stringifier.write(row));
-        stringifier.pipe(res);
-        return stringifier.end();
+
+    // res.attachment(`${report}.csv`);
+    const stringifier = csv({ header: true });
+
+    const headers = {
+      Authorization: `bearer ${req.user.access_token}`,
+      'Content-type': 'application/json'
+    };
+
+    fetch(`http://localhost:8089/reports/${report}`, { headers }).response
+      .then(response => {
+        pipeline(
+          response.body,
+          ndjson.parse(),
+          through.obj(process(report)),
+          stringifier,
+          res,
+          err => {
+            err && next(err);
+          }
+        );
       })
       .catch(next);
+
   });
 
   return router;
