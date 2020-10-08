8f11aa719ae0fb24fdb0eba858a5f6d93e033809
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-10-08 16:00:14 +0100

    add a link to the CatE pils csv

--- pages/downloads/views/index.jsx
+++ pages/downloads/views/index.jsx
@@ -9,21 +9,24 @@ const Index = () => {
     <h2>General downloads</h2>
 
     <h3><Link page="reporting" query={{ csv: 1 }} label="Completed tasks" /></h3>
-    <p>View a list of all completed tasks by type, which will be downloadable in a .csv file.</p>
+    <p>View a list of all completed tasks by type, which you can download as a .csv file.</p>
 
     <h3><Link page="downloads.report" report="named-people" label="Named people and admins" /></h3>
-    <p>View a list of contact details for all named people, admins, and HOLCs, which will be downloadable in a .csv file.</p>
+    <p>View a list of contact details for all named people, admins, and HOLCs, which you can download as a .csv file.</p>
 
     <h2>Personal licence downloads</h2>
 
-    <h3><Link page="downloads.report" report="pils" label="All PILs" /></h3>
-    <p>View a list of all active and revoked PILs, which will be downloadable in a .csv file.</p>
+    <h3><Link page="downloads.report" report="training-pils" label="Category E" /></h3>
+    <p>View a list of all category E PILs, which you can download as a .csv file.</p>
+
+    <h3><Link page="downloads.report" report="pils" label="All other categories" /></h3>
+    <p>View a list of all category A, B, C, D and F PILs, which you can download as a .csv file.</p>
 
     <h3><Link page="downloads.report" report="pil-reviews" label="Upcoming PIL reviews" /></h3>
-    <p>View a list of PILs which are due a review, which will be downloadable in a .csv file.</p>
+    <p>View a list of PILs which are due a review, which you can download as a .csv file.</p>
 
     <h3><Link page="downloads.report" report="completed-pil-reviews" label="Completed PIL reviews" /></h3>
-    <p>View a list of PIL reviews which have been submitted without changes, which will be downloadable in a .csv file.</p>
+    <p>View a list of PIL reviews which have been submitted without changes, which you can download as a .csv file.</p>
 
     <h2>Project licence downloads</h2>
 
@@ -37,7 +40,7 @@ const Index = () => {
     <p>View a list of all non-technical summaries by year, which will be downloadable in a .zip file.</p>
 
     <h3><Link page="downloads.report" report="ppl-sla" label="Expired statutory deadlines" /></h3>
-    <p>View a list of all PPL applications which passed their statutory deadline, which will be downloadable in a .csv file.</p>
+    <p>View a list of all PPL applications which passed their statutory deadline, which you can download as a .csv file.</p>
 
     <h2>Establishment licence downloads</h2>
 
