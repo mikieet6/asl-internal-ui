39853d1f7f4ff4c0aafd64d273d1aa4173049e74
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-12-07 15:52:34 +0000

    extract formatters to separate file, filter profiles on unassoc ests

---
+++ pages/search/formatters/index.jsx
@@ -0,0 +1,113 @@
+import React, { Fragment } from 'react';
+import sortBy from 'lodash/sortBy';
+import get from 'lodash/get';
+import { ExpiryDate, Link, Markdown } from '@asl/components';
+import ProjectSearchResult from '../views/components/project-search-result';
+import { projectTitle } from '@asl/pages/pages/common/formatters';
+import projectFormatters from '@asl/pages/pages/project/formatters';
+
+export default {
+  establishments: {
+    name: {
+      format: (name, establishment) => {
+        const highlight = get(establishment, `highlight.name[0]`);
+        const label = highlight ? <Markdown>{highlight}</Markdown> : name;
+        return <Link page="establishment.dashboard" establishmentId={establishment.id} label={label} />;
+      }
+    },
+    licenceNumber: {
+      format: (licenceNumber, establishment) => {
+        const highlight = get(establishment, `highlight.licenceNumber[0]`);
+        return highlight ? <Markdown>{highlight}</Markdown> : licenceNumber;
+      }
+    },
+    inspector: {
+      format: (inspector, establishment) => {
+        return establishment.asru.filter(p => p.asruInspector).map(profile => (
+          <p key={profile.id}>{`${profile.firstName} ${profile.lastName}`}</p>
+        ));
+      }
+    },
+    spoc: {
+      format: (spoc, establishment) => {
+        return establishment.asru.filter(p => p.asruLicensing).map(profile => (
+          <p key={profile.id}>{`${profile.firstName} ${profile.lastName}`}</p>
+        ));
+      }
+    }
+  },
+  profiles: {
+    name: {
+      format: (name, profile) => {
+        const firstName = get(profile, `highlight.firstName[0]`, profile.firstName);
+        const lastName = get(profile, `highlight.lastName[0]`, profile.lastName);
+        const label = <Markdown>{`${firstName} ${lastName}`}</Markdown>;
+        return <Link page="globalProfile" profileId={profile.id} label={label} />;
+      }
+    },
+    establishments: {
+      format: (establishments, profile) => {
+        if (profile.asruUser) {
+          return 'ASRU';
+        }
+        return sortBy(establishments, 'name').map(establishment => (
+          <p key={establishment.id}>
+            <Link page="establishment.dashboard" establishmentId={establishment.id} label={establishment.name} />
+          </p>
+        ));
+      }
+    }
+  },
+  projects: {
+    title: {
+      format: (title, project) => {
+        const highlight = get(project, 'highlight.title[0]');
+        const label = highlight ? <Markdown>{highlight}</Markdown> : projectTitle(project);
+        return (
+          <Fragment>
+            <Link page="project.read" establishmentId={project.establishment.id} projectId={project.id} label={label} />
+            { project.isLegacyStub ? ' - Partial record' : '' }
+          </Fragment>
+        );
+      }
+    },
+    licenceNumber: {
+      format: (licenceNumber, project) => {
+        const highlight = get(project, `highlight.licenceNumber[0]`);
+        return highlight ? <Markdown>{highlight}</Markdown> : licenceNumber;
+      }
+    },
+    establishment: {
+      format: (establishment, project) => {
+        const highlight = get(project, `highlight['establishment.name'][0]`);
+        const label = highlight ? <Markdown>{highlight}</Markdown> : establishment.name;
+        return <Link page="establishment.dashboard" establishmentId={establishment.id} label={label} />;
+      }
+    },
+    status: projectFormatters().status,
+    licenceHolder: {
+      format: ({ id, firstName, lastName }, project) => {
+        const highlight = get(project, `highlight['licenceHolder.lastName'][0]`);
+        const label = highlight ? <Markdown>{`${firstName} ${highlight}`}</Markdown> : `${firstName} ${lastName}`;
+        return <Link page="globalProfile" profileId={id} label={label} />;
+      }
+    },
+    expiryDate: {
+      format: (date, model) => {
+        if (!date || ['revoked', 'transferred'].includes(model.status)) {
+          return '-';
+        }
+        return <ExpiryDate
+          date={date}
+          dateFormat="D MMM YYYY"
+          showNotice={model.status === 'active' ? 11 : false}
+        />;
+      }
+    }
+  },
+  'projects-content': {
+    title: {
+      format: (title, project) => <ProjectSearchResult project={project} />
+    }
+  }
+};
