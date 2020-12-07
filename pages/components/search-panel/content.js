092971335b17d7349511b5c480e35fbe9b3f5e5f
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-12-07 15:54:36 +0000

    improved search labels and content

--- pages/components/search-panel/content.js
+++ pages/components/search-panel/content.js
@@ -2,18 +2,18 @@ module.exports = {
   title: 'Search',
   establishments: {
     title: 'Establishments',
-    label: 'Search by name',
-    viewAll: 'View all active establishments'
+    label: 'Search by establishment name, PEL number, inspector name or SPoC name',
+    viewAll: 'Clear search'
   },
   profiles: {
     title: 'People',
-    label: 'Search by name or licence number',
-    viewAll: 'View all people'
+    label: 'Search by name, email address or PIL number',
+    viewAll: 'Clear search'
   },
   projects: {
     title: 'Projects',
-    label: 'Search by name or licence number',
-    viewAll: 'View all active projects'
+    label: 'Search by title, primary establishment, PPL number or licence holder',
+    viewAll: 'Clear search'
   },
   'projects-content': {
     title: 'Project Content',
