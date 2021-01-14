6ae9d0916ba680d055951a68aadd0e62f40139d8
Leonard Martin <leonard.martin@gmail.com>
2021-01-14 11:22:32 +0000

    Simplify clear search link
    It's the same text in all cases, so there doesn't seem like much point in defining separately for all search types.

--- pages/components/search-panel/content.js
+++ pages/components/search-panel/content.js
@@ -1,19 +1,17 @@
 module.exports = {
   title: 'Search',
+  viewAll: 'Clear search',
   establishments: {
     title: 'Establishments',
-    label: 'Search by establishment name, PEL number, inspector name or SPoC name',
-    viewAll: 'Clear search'
+    label: 'Search by establishment name, PEL number, inspector name or SPoC name'
   },
   profiles: {
     title: 'People',
-    label: 'Search by name, email address or PIL number',
-    viewAll: 'Clear search'
+    label: 'Search by name, email address or PIL number'
   },
   projects: {
     title: 'Projects',
-    label: 'Search by title, primary establishment, PPL number or licence holder',
-    viewAll: 'Clear search'
+    label: 'Search by title, primary establishment, PPL number or licence holder'
   },
   'projects-content': {
     title: 'Project Content',
