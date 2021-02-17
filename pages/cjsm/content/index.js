b3b3c8e6ca67be3a9ef60c653a1a671fed82f5dc
Leonard Martin <leonard.martin@gmail.com>
2021-02-17 17:18:17 +0000

    Add basic email address validation for CJSM
    This prevent downstream errors if the value doesn't fit the prescribed format to be inserted into the database.

--- pages/cjsm/content/index.js
+++ pages/cjsm/content/index.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   errors: {
     cjsmEmail: {
-      required: 'Enter the CJSM email address'
+      required: 'Enter the CJSM email address',
+      match: 'Enter a valid email address'
     }
   },
   notifications: {
