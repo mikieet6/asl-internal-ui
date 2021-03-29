a32bbf49f9a195b8b79c4fadf5cac6d21945e238
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-03-29 17:13:46 +0100

    bump components for metric move

--- pages/reporting/views/components/task-counts.jsx
+++ pages/reporting/views/components/task-counts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Snippet, Link } from '@asl/components';
-import Number from './number';
+import { Snippet, Link, Number } from '@asl/components';
 
 const TaskCounts = ({ types }) => {
   const { tasks } = useSelector(state => state.static);
