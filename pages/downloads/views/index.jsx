79d6bb666f4696f8abaf69554603873229ee37e7
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-09-15 16:34:15 +0100

    update the content on the asru downloads page

--- pages/downloads/views/index.jsx
+++ pages/downloads/views/index.jsx
@@ -1,77 +1,84 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Header, Link } from '@asl/components';
 
-const Index = () => {
+export default function Index() {
+  return (
+    <div className="asru-downloads">
+      <Header title="Downloads"/>
 
-  return <Fragment>
-    <Header title="Downloads"/>
+      <section>
+        <h2>Project licences</h2>
 
-    <section>
-      <h2>Project licences</h2>
+        <Link page="downloads.report" report="ppl-list" label="Project licences" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all active and revoked project licences including duration, expiry date and establishment number.</p>
 
-      <h3><Link page="downloads.report" report="ppl-list" label="Project list" /></h3>
-      <p>Download a .csv file with summaries of the admin details of project licences such as expiry dates and statuses.</p>
+        <Link page="downloads.report" report="ppl-details" label="Project licence authorities" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all active and revoked project authorities including animal type, permissible purpose and highest severity.</p>
 
-      <h3><Link page="downloads.report" report="ppl-details" label="Project details" /></h3>
-      <p>Download a .csv file containing details of project licences, including animal types, number of protocols and use of GA animals etc.</p>
+        <Link page="downloads.report" report="ppl-conditions" label="Project conditions and authorisations" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all active and revoked project conditions and authorisations.</p>
 
-      <h3><Link page="downloads.report" report="ppl-conditions" label="Project conditions and authorisations" /></h3>
-      <p>Download a .csv file with summaries of the conditions and authorisations for each project.</p>
+        <Link page="downloads.report" report="ppl-protocols" label="Project protocols" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all active and revoked project protocols.</p>
 
-      <h3><Link page="downloads.report" report="ppl-protocols" label="PPL protocols" /></h3>
-      <p>Download a .csv file with a list of all protocols for each project.</p>
+        <Link page="downloads.report" report="ppl-applications" label="Project applications granted" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of new project licence applications granted in ASPeL, including a breakdown of time spent with the establishment and the Home Office.</p>
 
-      <h3><Link page="downloads.report" report="ppl-sla" label="Expired statutory deadlines" /></h3>
-      <p>View a list of all PPL applications which passed their statutory deadline, which you can download as a .csv file.</p>
+        <Link page="downloads.report" report="ppl-sla" label="Expired statutory deadlines" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of project licence applications that weren’t processed within the Home Office’s statutory deadline.</p>
+      </section>
 
-      <h3><Link page="downloads.report" report="ppl-applications" label="PPL Applications" /></h3>
-      <p>View a list of all PPL applications with a breakdown of time spent with the establishment and ASRU, which you can download as a .csv file.</p>
-    </section>
+      <section>
+        <h2>Personal licences</h2>
 
-    <section>
-      <h2>Personal licences</h2>
+        <Link page="downloads.report" report="pils" label="Category A, B, C, D and F licences" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all category A, B, C, D and F personal licences granted in ASPeL.</p>
 
-      <h3><Link page="downloads.report" report="training-pils" label="Category E" /></h3>
-      <p>View a list of all category E PILs, which you can download as a .csv file.</p>
+        <Link page="downloads.report" report="training-pils" label="Category E licences" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all category E personal licences granted in ASPeL.</p>
 
-      <h3><Link page="downloads.report" report="pils" label="All other categories" /></h3>
-      <p>View a list of all category A, B, C, D and F PILs, which you can download as a .csv file.</p>
+        <Link page="downloads.report" report="pil-reviews" label="Upcoming personal licence reviews" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all personal licences due a review.</p>
 
-      <h3><Link page="downloads.report" report="pil-reviews" label="Upcoming PIL reviews" /></h3>
-      <p>View a list of PILs which are due a review, which you can download as a .csv file.</p>
+        <Link page="downloads.report" report="completed-pil-reviews" label="Completed personal licence reviews" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of personal licences that have been reviewed with no changes to the licence.</p>
+      </section>
 
-      <h3><Link page="downloads.report" report="completed-pil-reviews" label="Completed PIL reviews" /></h3>
-      <p>View a list of PIL reviews which have been submitted without changes, which you can download as a .csv file.</p>
-    </section>
+      <section>
+        <h2>Establishment licences</h2>
 
-    <section>
-      <h2>Establishment licences</h2>
+        <Link page="downloads.report" report="establishments" label="Establishment licences" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list of all active and revoked establishment licences including details of the licence holder, Home Office Liaison Contact and number of active projects.</p>
 
-      <h3><Link page="downloads.report" report="establishments" label="Establishment summaries" /></h3>
-      <p>Download a .csv file with summaries of every establishment (name, licence number, PELH, Spocs, Inspectors, number of active PPLs, etc).</p>
+        <Link page="downloads.report" report="establishment-conditions" label="Establishment conditions and authorisations" />
+        <p className="file-type-label">CSV</p>
+        <p>Download a list active and revoked establishment conditions and authorisations.</p>
+      </section>
 
-      <h3><Link page="downloads.report" report="establishment-conditions" label="Establishment conditions and authorisations" /></h3>
-      <p>Download a .csv file with the conditions and authorisations of every establishment (name, licence number, status, conditions, authorisations).</p>
-    </section>
+      <section>
+        <h2>Establishments users</h2>
 
-    <section>
-      <h2>Establishments users</h2>
-    </section>
+        <Link page="downloads.report" report="named-people" label="Named people and admins" />
+        <p className="file-type-label">CSV</p>
+        <p>View a list of contact details for all named people, admins, and HOLCs, which you can download as a .csv file.</p>
+      </section>
 
-    <section>
-      <h2>Other downloads</h2>
-    </section>
+      <section>
+        <h2>Other downloads</h2>
+        <p>You can <Link page="reporting" query={{ csv: 1 }} label="view and download more data on tasks completed in ASPeL" />.</p>
+      </section>
 
-    <h3><Link page="reporting" query={{ csv: 1 }} label="Completed tasks" /></h3>
-    <p>View a list of all completed tasks by type, which you can download as a .csv file.</p>
-
-    <h3><Link page="downloads.report" report="named-people" label="Named people and admins" /></h3>
-    <p>View a list of contact details for all named people, admins, and HOLCs, which you can download as a .csv file.</p>
-
-    <h3><Link page="downloads.report" report="newsletter-subscriptions" label="Operational newsletter subscriptions" /></h3>
-    <p>Download a .csv file containing a list of users who are subscribed to the operational newsletter.</p>
-
-  </Fragment>;
-};
-
-export default Index;
+    </div>
+  );
+}
