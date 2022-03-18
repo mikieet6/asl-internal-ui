4c5b0427e960d92d49fbbf0ed7dfdb1c64b120a2
Leonard Martin <leonard.martin@gmail.com>
2022-03-18 10:55:07 +0000

    Update html structure of metrics page to support functional testing

--- pages/reporting/views/index.jsx
+++ pages/reporting/views/index.jsx
@@ -31,92 +31,101 @@ export default function Index() {
 
     <hr/>
 
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h2>Tasks completed</h2>
-        <p><DateRange /></p>
+    <section>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h2>Tasks completed</h2>
+          <p><DateRange /></p>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-one-third">
-        <Metric number={tasks.total} label="Tasks completed" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-third">
+          <Metric number={tasks.total} label="Tasks completed" className="tasks-completed" />
+        </div>
+        <div className="govuk-grid-column-one-third">
+          <Metric number={tasks.ppls} label="Project licences granted" className="ppls-granted" />
+        </div>
+        <div className="govuk-grid-column-one-third">
+          <Metric number={tasks.iterations / (tasks.ppls || 1)} label="Iterations of project applications" className="ppl-iterations" />
+        </div>
       </div>
-      <div className="govuk-grid-column-one-third">
-        <Metric number={tasks.ppls} label="Project licences granted" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <p><Link page="reporting.details.completedTasks" label="View more data on completed tasks" /></p>
+        </div>
       </div>
-      <div className="govuk-grid-column-one-third">
-        <Metric number={tasks.iterations / (tasks.ppls || 1)} label="Iterations of project applications" />
-      </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <p><Link page="reporting.details.completedTasks" label="View more data on completed tasks" /></p>
-      </div>
-    </div>
+    </section>
 
     <hr/>
 
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h2>Tasks with missed deadlines</h2>
-        <p><DateRange /></p>
+    <section>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h2>Tasks with missed deadlines</h2>
+          <p><DateRange /></p>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-one-half">
-        <Metric number={deadlines} label="Missed statutory deadline" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-half">
+          <Metric number={deadlines} label="Missed statutory deadline" className="statutory-deadlines" />
+        </div>
+        <div className="govuk-grid-column-one-half">
+          <Metric number={internalDeadlines.total} label="Missed internal target" className="internal-targets" />
+        </div>
       </div>
-      <div className="govuk-grid-column-one-half">
-        <Metric number={internalDeadlines.total} label="Missed internal target" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <p><Link page="reporting.details.deadlines" label="View more data on tasks with missed deadlines" /></p>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <p><Link page="reporting.details.deadlines" label="View more data on tasks with missed deadlines" /></p>
-      </div>
-    </div>
+    </section>
 
     <hr/>
 
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h2>Task processing by staff</h2>
+    <section>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h2>Task processing by staff</h2>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-one-half">
-        <Metric number={tasksOutstanding.total} label="Outstanding tasks" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-half">
+          <Metric number={tasksOutstanding.total} label="Outstanding tasks" />
+        </div>
+        <div className="govuk-grid-column-one-half">
+          <Metric number={tasksOutstanding.unassigned} label="Unassigned tasks" />
+        </div>
       </div>
-      <div className="govuk-grid-column-one-half">
-        <Metric number={tasksOutstanding.unassigned} label="Unassigned tasks" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <p><Link page="asruWorkload" label="View more data on task processing by staff" /></p>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <p><Link page="asruWorkload" label="View more data on task processing by staff" /></p>
-      </div>
-    </div>
+    </section>
 
     <hr/>
 
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h2>Number of current active licences</h2>
+    <section>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h2>Number of current active licences</h2>
+        </div>
       </div>
-    </div>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-one-quarter">
-        <Metric number={licences.establishments} label="Establishments" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-quarter">
+          <Metric number={licences.establishments} label="Establishments" />
+        </div>
+        <div className="govuk-grid-column-one-quarter">
+          <Metric number={licences.projects} label="Projects" />
+        </div>
+        <div className="govuk-grid-column-one-quarter">
+          <Metric number={licences.pils} label="PIL A, B, C, D, F" />
+        </div>
+        <div className="govuk-grid-column-one-quarter">
+          <Metric number={licences.trainingPils} label="PIL E" />
+        </div>
       </div>
-      <div className="govuk-grid-column-one-quarter">
-        <Metric number={licences.projects} label="Projects" />
-      </div>
-      <div className="govuk-grid-column-one-quarter">
-        <Metric number={licences.pils} label="PIL A, B, C, D, F" />
-      </div>
-      <div className="govuk-grid-column-one-quarter">
-        <Metric number={licences.trainingPils} label="PIL E" />
-      </div>
-    </div>
+    </section>
+
   </Fragment>;
 }
